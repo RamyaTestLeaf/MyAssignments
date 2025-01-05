0d3791939784499915617ee8a16fdb99524ecd9e
RamyaTestLeaf <ramyavignesh.8985@gmail.com>
2025-01-06 00:29:44 +0530

    Add files via upload
    week1 assignments

---
+++ week1/Chrome.java
@@ -0,0 +1,17 @@
+package week1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCutKey = 'C';
+		System.out.println("version"+":"+version+"\n"+"developer"+":"+developer+"\n"+"isBeta"+":"+isBeta);
+		
+		System.out.println("releaseYear"+":"+releaseYear+"\n"+"shortCutKey"+":"+shortCutKey);		
+
+	}
+
+}
